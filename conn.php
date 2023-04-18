0184fa3e1b4b5068b6c4ab92d07f1b0542f6180c
GreenVenom77 <hosome2222@gmail.com>
2023-04-18 20:01:35 +0200

    Database connection

---
+++ conn.php
@@ -0,0 +1,11 @@
+<?php
+    $con=pg_connect("host=database-1.cxjhu3idchez.eu-north-1.rds.amazonaws.com port=5432 dbname=Grays user=postgres password=teamgrays");
+    if($con)
+    {
+        echo "connection success";
+    }
+    else
+    {
+        echo "connection failure";
+    }
+?>
--- index.php
+++ index.php
@@ -2,8 +2,19 @@
 <html>
 <head>
     <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="Assets/css/style.css">
 </head>
+<?php
+    $myPDO = new PDO('pgsql:host=database-1.cxjhu3idchez.eu-north-1.rds.amazonaws.com; dbname=Grays', 'postgres', 'teamgrays');
+    if(!$myPDO)
+    {
+        echo "connection failed";
+    }
+    else
+    {
+        echo "connection success";
+    }
+?>
 <body>
     <form action="login.php" method="post">
         <h2>Login</h2>
@@ -14,9 +25,9 @@
         <input type="text" name="uname" placeholder="User Name"><br>
 
         <label>Password</label>
-        <input type="Password" name="Password" placeholder="Password"> <br>
+        <input type="Password" name="Password" placeholder="Password"><br>
 
         <button type="submit">Login</button>
 </form>
 </body>
-</html>    
+</html>
