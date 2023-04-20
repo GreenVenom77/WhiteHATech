e4b54a5e7a04805ee67c630bf9a2ceda5f23bd24
GreenVenom77 <hosome2222@gmail.com>
2023-04-20 20:37:56 +0200

    new connection

--- conn.php
+++ conn.php
@@ -1,7 +1,10 @@
 <?php
-    $con=pg_connect("host=database-1.cxjhu3idchez.eu-north-1.rds.amazonaws.com port=5432 dbname=Grays user=postgres password=teamgrays");
-    if(!$con)
-    {
-        echo "connection fail";
-    }
-?>
+
+    session_start();
+
+    $DB_Host = 'database-1.cxjhu3idchez.eu-north-1.rds.amazonaws.com';
+    $DB_Name = 'Grays';
+    $DB_User = 'postgres';
+    $DB_Password = 'teamgrays';
+
+    $con = pg_connect("host=$DB_Host dbname=$DB_Name user=$DB_User password=$DB_Password");
